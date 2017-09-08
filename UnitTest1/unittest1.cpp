6a249fc275da936282e29422ed33f67edd6c3ab2
Захар Владимирович Малиновский <z.malinovskiy@ubc.local>
2017-09-08 16:46:02 +0300

    unit test LoadLine added

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -1,17 +1,31 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include "../DeleteOldFilesCxx11/row2.h"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+template<> static
+std::wstring 
+	Microsoft::VisualStudio::CppUnitTestFramework::
+ToString<my::LoadLineRetvalue> (const my::LoadLineRetvalue & r){
+	return std::to_wstring(static_cast<int>(r));
+}
 
 namespace UnitTest1
 {		
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(TestMethod_LoadLine)
 		{
 			// TODO: Your test code here
+			my::row			one_row;
+			std::string		line1{ "//comment" };
+			Assert::AreEqual (one_row.LoadLine(line1),	my::LoadLineRetvalue::ignore);
+			//Assert::AreEqual(0, 0);
+
 		}
 
 	};
